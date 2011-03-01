c83dd92f27094da1837d591a6e16a43a73657313
David Lawrence Miller <dave@ninepointeightone.net>
2011-03-01 12:14:32 +0000

    more bits...

--- R/rxmpp.R
+++ R/rxmpp.R
@@ -1,16 +1,15 @@
 # RXMPP
 
-# add something for a persistant connection?
-
 # get the settings and set up the connection object
 shout_settings<-function(username, password=NULL, recipient=NULL, server="talk.google.com", 
                         test.message="This is a test of RXMPP."){
 
-   # if the password is NULL then ask for it
-   if(is.null(password)){
-      cat("No password supplied.\nPassword:")
-      password<-scan()
-   }
+   ## this echoes the password back at the moment -- BAD
+   ## if the password is NULL then ask for it
+   #if(is.null(password)){
+   #   cat("No password supplied.\nPassword:")
+   #   password<-readLines(n=1)
+   #}
 
    # create connection object
    conn<-list(username=username,password=password)
@@ -30,13 +29,12 @@ shout<-function(message,recipient,conn){
    # send the message using conn
    dyn.load("wocky-message")
    
-   wood_ret<-.C("rxmpp_send",
+   send_obj<-.C("rxmpp_send",
                 in_jid=as.character(conn$username),
                 in_password=as.character(conn$password),
                 in_recipient=as.character(recipient),
                 in_message=as.character(message))
    
    dyn.unload("wocky-message")
-
 
 }
